66f5cd28d09d6c560fa8e4254b2a4c7a9e0738e9
unknown <megee@697-334-Waymoot.sf.k12.internal>
2015-09-02 11:38:41 -0700

    Added first name to index

--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -15,38 +15,56 @@ import java.io.IOException;
 public class Lightning extends PApplet {
 
 int startX=150;
-int startY=0;
+int startY=60;
 int endX=150;
 int endY=0;
-
+int cloudX= 100;
+int cloudY= 50;
 public void setup()
 {
-  size(300, 300);
+  size(500, 400);
   strokeWeight(3);
   background(0,0,0);
 }
 
 public void draw()
 {
+
+	cloud();
 	stroke((int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256));
-	while(endX<300)
+	while(endY<500)
 	{
-		endX= startX+ (int)(Math.random()*9);
-		endY=startY+ (int)(Math.random()*18)-9;
+		endY= startY+ (int)(Math.random()*9);
+		endX = startX + (int)(Math.random()*18)-9;
 		line(startX, startY,endX, endY);
-		startX=endX;
 		startY=endY;
+		startX=endX;
 
 	}
+}
 
+public void cloud()
+{
+	stroke(139, 131, 120);
+	fill(139, 131, 120);
+	ellipse (cloudX+15, cloudY-15, 175, 70);
+	ellipse(cloudX-30, cloudY+15, 175, 50);
+	ellipse(cloudX+60, cloudY+15, 175, 50);
+	ellipse (cloudX+15, cloudY+25, 175, 70);
+	//second cloud
+	ellipse (cloudX+270, cloudY-15, 175, 70);
+	ellipse(cloudX+235, cloudY+15, 175, 50);
+	ellipse(cloudX+315, cloudY+15, 175, 50);
+	ellipse (cloudX+270, cloudY+25, 175, 70);
 }
 public void mousePressed()
 {
-	startX=150;
-	startY=0;
-
+	startX=(int)(Math.random()*500);
+	startY=60;
 	endX=150;
 	endY=0;
+	background((int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256));
+	
 
 
 }
